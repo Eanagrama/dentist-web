913acd2075488a2ca875cf6ff3ab92dadd0733a7
anagrama <electronic.anagrama@gmail.com>
2019-11-11 23:25:24 -0500

    Template added
    Template added to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { IntroCarouselComponent } from './intro-carousel/intro-carousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServicesComponent } from './services/services.component';
+import { ClientsComponent } from './clients/clients.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TopBarComponent,
+    HeaderComponent,
+    IntroCarouselComponent,
+    FooterComponent,
+    ServicesComponent,
+    ClientsComponent,
+    TeamComponent,
+    ContactComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
